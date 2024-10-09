5a6ce9a5ac499235e47c46c4c6d333222194a625
opexu <opexu1986@gmail.com>
2024-10-10 01:30:20 +0300

    empty

--- front/src/types/account.ts
+++ front/src/types/account.ts
@@ -1,10 +1,10 @@
 export interface IXAccount {
     id: number;
-    name: string;
-    followers_number: number;
-    avatar_url: string;
-    average_views: number;
-    price: number;
+    name: string; // +
+    followers_number: number; // +
+    avatar_url: string; // +
+    average_views: number; // +
+    price: number; // +
     description?: string;
     king?: boolean;
     flash?: boolean;
