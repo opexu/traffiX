c306fefe4c7e039684b1a80d59a29e370785e22f
Evgeny Smirnov <evgeny.smirnov@onlinesup.com>
2024-10-09 22:09:57 +0300

    separated blockchain service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SolanaModule } from './solana/solana.module';
-import { CronModule } from './cron/cron.module';
 import { ConfigModule } from '@nestjs/config';
 import { AccountModule } from './account/account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
-    SolanaModule,
-    CronModule,
     ConfigModule.forRoot(),
     AccountModule,
     TypeOrmModule.forRoot({
@@ -24,7 +19,6 @@ import { TransactionModule } from './transaction/transaction.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
-    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
