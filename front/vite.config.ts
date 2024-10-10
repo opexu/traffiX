4868f54b9dfad5d4fbbbaebd7fa79272a1fe4fb8
opexu <opexu1986@gmail.com>
2024-10-10 20:44:59 +0300

    buffer

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+
 /** @type {import('vite').UserConfig} */
 export default defineConfig( ( { command, mode, isSsrBuild, isPreview } ) => {
     
@@ -13,6 +16,17 @@ export default defineConfig( ( { command, mode, isSsrBuild, isPreview } ) => {
     return {
         plugins: [
             vue(),
+            nodePolyfills( {
+                include: [],
+                exclude: [],
+                globals: {
+                    Buffer: true,
+                    global: true,
+                    process: false,
+                },
+                overrides: {},
+                protocolImports: false,
+            } ),
         ],
         root: path.resolve( __dirname, './src' ),
         resolve: {
