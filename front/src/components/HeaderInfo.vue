798d92edf0ae11152eb180b8415b110de3b14233
opexu <opexu1986@gmail.com>
2024-10-11 20:41:58 +0300

    one word

--- front/src/components/HeaderInfo.vue
+++ front/src/components/HeaderInfo.vue
@@ -29,7 +29,7 @@
             >[ Go back ]</button>
             <div class="max-w-xl h-fit py-16 px-8 gap-8 rounded-3xl flex flex-col items-start justify-start bg-slate-950">
                 <h1 class="text-3xl">How it works:</h1>
-                <p class="text-sol-400">Spot is a scam free platform, where all crypto payments are safe</p>
+                <p class="text-sol-400">TraffiX is a scam free platform, where all crypto payments are safe</p>
                 <p><span class="text-slate-300">Step</span> <span class="text-sol-400">1:</span> Pick influencer that you like</p>
                 <p><span class="text-slate-300">Step</span> <span class="text-sol-400">2:</span> Make a post</p>
                 <p><span class="text-slate-300">Step</span> <span class="text-sol-400">3:</span> Watch your token pump</p>
